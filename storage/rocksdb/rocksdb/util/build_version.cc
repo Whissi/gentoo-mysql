918c599705563598901661d32965df8fdeae6392
Thomas Deutschmann <whissi@gentoo.org>
2019-12-05 21:29:03 +0100

    Import Percona-Server 8.0.18-9
    Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

---
+++ storage/rocksdb/rocksdb/util/build_version.cc
@@ -0,0 +1,6 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha =
+"rocksdb_build_git_sha:3f5c1236026233b34be64d597fe660d7e698f2f5";
+const char* rocksdb_build_git_date =
+"rocksdb_build_git_date:2019-12-05";
+const char* rocksdb_build_compile_date = __DATE__;
