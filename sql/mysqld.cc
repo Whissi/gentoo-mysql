2bf0e8d40f8ef813ee45ad4d3c488184ca884dc6
Thomas Deutschmann <whissi@gentoo.org>
2020-04-30 23:00:09 +0200

    Add LibreSSL support
    Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3419,7 +3419,7 @@ int warn_self_signed_ca()
 static int init_ssl()
 {
 #ifdef HAVE_OPENSSL
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
   CRYPTO_malloc_init();
 #else /* OPENSSL_VERSION_NUMBER < 0x10100000L */
   OPENSSL_malloc_init();
